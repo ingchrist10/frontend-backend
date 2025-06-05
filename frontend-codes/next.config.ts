bd1fe75c9e89ec19fcd36a82c6f3e886324aedfc
ingchrist10 <ingchrist10@gmail.com>
2025-06-05 11:59:36 +0000

    ok

--- frontend-codes/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000"],
-};
-
-export default nextConfig;
